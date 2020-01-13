db8c7cd65f034753cb8334d24a94fa5077f0962f
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-13 17:16:42 +0800

    Alpha Miner algo csv event log

--- components/docker/SPAlphaMiner.py
+++ components/docker/SPAlphaMiner.py
@@ -2,22 +2,28 @@
 from __future__ import absolute_import, print_function
 
 import os
-from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.algo.discovery.alpha import factory as alpha_miner
 from pm4py.visualization.petrinet import factory as pn_vis_factory
+from pm4py.objects.petri.exporter import pnml as pnml_exporter
 from suanpan.app import app
-from suanpan.app.arguments import Folder
+from suanpan.app.arguments import Folder, Csv
+from utils.csv import convert_df_pm_format
 
 
-@app.input(Folder(key="inputData"))
-@app.output(Folder(key="outputData"))
+@app.input(Csv(key="inputData1", alias="inputData"))
+@app.output(Folder(key="outputData1", alias="outputData"))
 def SPAlphaMiner(context):
     args = context.args
-    inputFile = os.path.join(args.inputData, os.listdir(args.inputData)[0])
-    log = xes_importer.import_log(inputFile)
+    log = convert_df_pm_format(args.inputData)
     net, initial_marking, final_marking = alpha_miner.apply(log)
     gviz = pn_vis_factory.apply(net, initial_marking, final_marking)
     pn_vis_factory.save(gviz, os.path.join(args.outputData, "alpha-miner.png"))
+    pnml_exporter.export_net(
+        net,
+        initial_marking,
+        os.path.join(args.outputData, "petri_final.pnml"),
+        final_marking=final_marking,
+    )
 
     return args.outputData
 
