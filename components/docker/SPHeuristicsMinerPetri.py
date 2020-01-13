dae7ecc9045ff470cec15e5428360a68088ad569
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-13 17:17:31 +0800

    heuristics miner algo

---
+++ components/docker/SPHeuristicsMinerPetri.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import os
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.algo.discovery.heuristics import factory as heuristics_miner
+from pm4py.visualization.petrinet import factory as pn_vis_factory
+from suanpan.app import app
+from suanpan.app.arguments import Folder, Float, Int
+
+
+@app.input(Folder(key="inputData1", alias="inputData"))
+@app.param(Float(key="dependencyThresh", default=0.5))
+@app.param(Float(key="andMeasureThresh", default=0.65))
+@app.param(Int(key="minActCount", default=1))
+@app.param(Int(key="minDfgOccurrences", default=1))
+@app.param(Float(key="dfgPreCleaningNoiseThresh", default=0.05))
+@app.output(Folder(key="outputData1", alias="outputData"))
+def SPHeuristicsMiner(context):
+    args = context.args
+    inputFile = os.path.join(args.inputData, os.listdir(args.inputData)[0])
+    log = xes_importer.import_log(inputFile)
+    net, im, fm = heuristics_miner.apply(
+        log,
+        parameters={
+            "dependency_thresh": args.dependencyThresh,
+            "and_measure_thresh": args.andMeasureThresh,
+            "min_act_count": args.minActCount,
+            "min_dfg_occurrences": args.minDfgOccurrences,
+            "dfg_pre_cleaning_noise_thresh": args.dfgPreCleaningNoiseThresh,
+        },
+    )
+    gviz = pn_vis_factory.apply(net, im, fm)
+    pn_vis_factory.save(gviz, os.path.join(args.outputData, "heuristics-miner.png"))
+
+    return args.outputData
+
+
+if __name__ == "__main__":
+    SPHeuristicsMiner()
